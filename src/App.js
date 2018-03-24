d9c8a2ed7d32572d19320d06ca11ab0360eb924a
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2018-03-24 16:57:23 +0000

    Use styled-components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "./App.css";
 
 import List from "./pages/List";
 import Upload from "./pages/Upload";
 import View from "./pages/View";
 
+const Main = styled.main`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #222;
+  height: auto;
+  padding: 40px;
+  color: white;
+  font-size: 35px;
+  letter-spacing: 15px;
+`;
+
+const Navigation = styled.nav`
+  ul {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    margin: 0;
+    padding: 10px;
+  }
+`;
+
 const App = () => (
   <Router>
-    <div className="App">
-      <header className="App-header">🐭🐶🐱🐌🙊🐣</header>
+    <Main>
+      <Header>🐭🐶🐱🐌🙊🐣</Header>
 
-      <nav className="App-nav">
+      <Navigation>
         <ul>
           <li>
             <Link to="/">List</Link>
@@ -20,12 +47,12 @@ const App = () => (
             <Link to="/upload">Upload</Link>
           </li>
         </ul>
-      </nav>
+      </Navigation>
 
       <Route exact path="/" component={List} />
       <Route path="/upload" component={Upload} />
       <Route path="/view/:id" component={View} />
-    </div>
+    </Main>
   </Router>
 );
 
